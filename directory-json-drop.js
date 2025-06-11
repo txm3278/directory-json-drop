9098a9454e959634d35614f90e443a983ff8d220
Tom Mineo <tmineo98@gmail.com>
2025-06-11 10:15:14 -0400

    Update for v13

--- directory-json-drop.js
+++ directory-json-drop.js
@@ -1,7 +1,9 @@
-Hooks.on('renderSidebarTab', (app, html)=>{
-  html.find('.directory-list').on('drop', async function(event){
-          event.originalEvent.preventDefault();
-          const files = event.originalEvent.dataTransfer.files;
+Hooks.on('renderAbstractSidebarTab', (app, html)=>{
+  const directoryList = html.querySelector('.directory-list');
+  if (!directoryList) return;
+  directoryList.addEventListener('drop', async function(event){
+          event.preventDefault();
+          const files = event.dataTransfer.files;
           if (!files[0]) return true;
           for (let file of files) {
             const reader = new FileReader();
@@ -10,6 +12,5 @@ Hooks.on('renderSidebarTab', (app, html)=>{
             }, false);
             reader.readAsText(file);
           }
-          
   });
 });
